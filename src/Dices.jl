7de0444ee14c61b5675fe879f4325823f1aabf34
Abel <abel.s.siqueira@gmail.com>
2020-07-20 23:35:16 -0300

    :white_check_mark: 100% coverage

--- src/Dices.jl
+++ src/Dices.jl
@@ -1,8 +1,12 @@
 module Dices
 
+export coin
+
 include("dice.jl")
 include("roll.jl")
 include("operations.jl")
 include("histogram.jl")
+
+const coin = Dice(2) - 1
 
 end # module
--- test/basic.jl
+++ test/basic.jl
@@ -1,10 +1,10 @@
 @testset "Basic tests - making sure basic things don't throw errors" begin
-    @test 1 ≤ roll(d20) ≤ 20
-    @test 1 ≤ roll(Roll(d20)) ≤ 20
-    @test 2 ≤ roll(2d20) ≤ 40
-    @test 4 ≤ roll(2d20 + 2) ≤ 42
-    @test 2 ≤ roll(d8 + d6) ≤ 14
-    @test 5 ≤ roll(3d4 + 2d6) ≤ 24
-    @test 4 ≤ roll(3d4 + d6) ≤ 18
-    @test 3 ≤ roll(drop(4d6)) ≤ 18
+  @test 1 ≤ roll(d20) ≤ 20
+  @test 1 ≤ roll(Roll(d20)) ≤ 20
+  @test 2 ≤ roll(2d20) ≤ 40
+  @test 4 ≤ roll(2d20 + 2) ≤ 42
+  @test 2 ≤ roll(d8 + d6) ≤ 14
+  @test 5 ≤ roll(3d4 + 2d6) ≤ 24
+  @test 4 ≤ roll(3d4 + d6) ≤ 18
+  @test 3 ≤ roll(drop(4d6)) ≤ 18
 end
