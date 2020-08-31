8c5c2af8287ab3128a190f68b5859f9f411add0b
Abel Siqueira <abel.s.siqueira@gmail.com>
2020-08-31 18:24:18 +0000

    Add another example
    This code was modified inside codespaces.

--- examples/4d6D1.jl
+++ examples/4d6D1.jl
@@ -1,8 +1,16 @@
 using DiceRolls, Plots
+gr()
 
 begin
   v, f = DiceRolls.histogram(drop(4d6), normalize=true)
   Plots.bar(v, f, size=(400,300), leg=false)
   xticks!(v)
   png("4d6D1")
+end
+
+begin
+  v, f = DiceRolls.histogram((1d4 - 1) * (1d4 - 1))
+  Plots.bar(v, f, size=(400,300), leg=false)
+  xticks!(v)
+  png("complex")
 end
